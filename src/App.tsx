b325797e32fe8c7b9a077c58001f8623c0498e45
Luiz João Motta <lmotta@redhat.com>
2021-08-29 22:07:23 -0300

    Add linter and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
-
 import * as React from "react";
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
+import { FormPage } from "./FormPage";
+import { HomePage } from "./HomePage";
+import { LoginPage } from "./LoginPage";
 
 export function App() {
-    return (<div><p>something</p></div>)
+  return (
+    <HashRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/form">Form</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+          <Route path="/form">
+            <FormPage />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </HashRouter>
+  );
 }
---
+++ src/FormPage.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+
+export function FormPage() {
+  return <div>form</div>;
+}
